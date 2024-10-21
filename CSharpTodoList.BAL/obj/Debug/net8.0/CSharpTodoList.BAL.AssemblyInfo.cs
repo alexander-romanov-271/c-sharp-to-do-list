20b9b8381061fa6680e133d3989afcffd0e46be8
Alexander Romanov <alexander.romanov.271@gmail.com>
2024-10-21 23:49:57 +0300

    One more change

--- CSharpTodoList.BAL/obj/Debug/net8.0/CSharpTodoList.BAL.AssemblyInfo.cs
+++ CSharpTodoList.BAL/obj/Debug/net8.0/CSharpTodoList.BAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpTodoList.BAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec1a6ac089dbb971191286b09aa10f6317bc1882")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a57d4a2751596fae473a49e7f6c6daae98718d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpTodoList.BAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpTodoList.BAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CSharpTodoList.DAL/Class1.cs
+++ CSharpTodoList.DAL/Class1.cs
@@ -1,6 +1,26 @@
-﻿namespace CSharpTodoList.DAL;
+﻿using System.Collections.Immutable;
+using System.IO;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
-public class Class1
+namespace CSharpTodoList.DAL;
+
+public class FileManager
 {
+    public static void WriteFile(string text, string path, bool append = true)
+    {
+        using (StreamWriter sw = new StreamWriter(path, append))
+        {
+            sw.WriteLine(text);
+        }
+    }
 
+    public static string ReadFile(string path)
+    {
+        using (StreamReader sr = new StreamReader(path))
+        {
+            string text = sr.ReadToEnd();
+            return text;
+        }
+    }
 }

1d40ba4cbb70c3c0aca2fea3f20b965f1a23bba1
Alexander Romanov <alexander.romanov.271@gmail.com>
2024-10-21 23:48:16 +0300

    one more commit

---
+++ CSharpTodoList.DAL/Models/FileModel.cs
@@ -0,0 +1,10 @@
+using CSharpTodoList.DAL.Models;
+
+namespace CSharpTodoList.DAL.Models
+{
+    public class FileModel
+    {
+        public int id { get; set; }
+
+    }
+}
---
+++ CSharpTodoList.DAL/Models/TodoListModel.cs
@@ -0,0 +1,9 @@
+namespace CSharpTodoList.DAL.Models
+{
+    public class TodoListModel
+    {
+        public int Id { get; set; }
+        public string Name {get; set;} = "";
+
+    }
+}

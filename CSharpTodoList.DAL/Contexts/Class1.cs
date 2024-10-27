4aee616b2aafbaf95e8dcfa35a87ab1d46e31575
Alexander Romanov <alexander.romanov.271@gmail.com>
2024-10-27 19:21:37 +0300

    Delete Assembly info files for each project from git tracking.\nAdd CSharpTodolist/Contexts

--- CSharpTodoList.DAL/Contexts/Class1.cs
+++ CSharpTodoList.DAL/Contexts/Class1.cs
@@ -4,18 +4,18 @@ using System.Runtime.CompilerServices;
 
 namespace CSharpTodoList.DAL.Contexts;
 
-/*class FileContext : IDisposable
+class FileContext : IDisposable
 {
     protected StreamWriter? Conn {get; set;}
     public static StreamWriter Open(string? path = null, bool append = true)
     {
         string workingpath = path ?? Directory.GetCurrentDirectory() + "/projectslist.txt";
-        Conn = new StreamWriter(workingpath, append);  
+        Conn = new StreamWriter(workingpath, append);
         return Conn;
     }
 
     public void Dispose()
     {
-        ;
+        
     }
-}*/
+}
